1f1a180dcd263df0b4e5e2214b626b7862a81eae
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2023-05-20 14:12:08 +0300

    editing student form

--- University-Library-Management/student_form.Designer.cs
+++ University-Library-Management/student_form.Designer.cs
@@ -36,10 +36,10 @@
             // 
             this.sign_up_std.BackColor = System.Drawing.SystemColors.Desktop;
             this.sign_up_std.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sign_up_std.Location = new System.Drawing.Point(131, 130);
-            this.sign_up_std.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.sign_up_std.Location = new System.Drawing.Point(175, 160);
+            this.sign_up_std.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.sign_up_std.Name = "sign_up_std";
-            this.sign_up_std.Size = new System.Drawing.Size(230, 102);
+            this.sign_up_std.Size = new System.Drawing.Size(307, 126);
             this.sign_up_std.TabIndex = 0;
             this.sign_up_std.Text = "Sign Up";
             this.sign_up_std.UseVisualStyleBackColor = false;
@@ -49,10 +49,10 @@
             // 
             this.sign_in.BackColor = System.Drawing.SystemColors.Desktop;
             this.sign_in.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sign_in.Location = new System.Drawing.Point(494, 130);
-            this.sign_in.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.sign_in.Location = new System.Drawing.Point(659, 160);
+            this.sign_in.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.sign_in.Name = "sign_in";
-            this.sign_in.Size = new System.Drawing.Size(216, 102);
+            this.sign_in.Size = new System.Drawing.Size(288, 126);
             this.sign_in.TabIndex = 1;
             this.sign_in.Text = "Sign In";
             this.sign_in.UseVisualStyleBackColor = false;
@@ -60,19 +60,18 @@
             // 
             // student_form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ClientSize = new System.Drawing.Size(840, 366);
+            this.ClientSize = new System.Drawing.Size(1120, 450);
             this.Controls.Add(this.sign_in);
             this.Controls.Add(this.sign_up_std);
             this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "student_form";
             this.ShowIcon = false;
             this.Text = "Student Form";
-            this.TransparencyKey = System.Drawing.Color.Black;
-            //this.Load += new System.EventHandler(this.student_form_Load);
+            this.TransparencyKey = System.Drawing.Color.Transparent;
             this.ResumeLayout(false);
 
         }
