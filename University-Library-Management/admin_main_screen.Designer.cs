081e2a27ae2867e61178597d2dc0fecfb7dd75f0
Nour El-Din Ahmed Hussein <nour.sehs.3@gmail.com>
2023-05-21 16:42:05 +0300

    merge view visits with add visit

--- University-Library-Management/admin_main_screen.Designer.cs
+++ University-Library-Management/admin_main_screen.Designer.cs
@@ -29,33 +29,22 @@
         private void InitializeComponent()
         {
             this.view_visit = new System.Windows.Forms.Button();
-            this.add_visit = new System.Windows.Forms.Button();
             this.books_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // view_visit
             // 
-            this.view_visit.Location = new System.Drawing.Point(212, 168);
+            this.view_visit.Location = new System.Drawing.Point(96, 168);
             this.view_visit.Name = "view_visit";
             this.view_visit.Size = new System.Drawing.Size(157, 74);
             this.view_visit.TabIndex = 0;
-            this.view_visit.Text = "View all Visits";
+            this.view_visit.Text = "Visits";
             this.view_visit.UseVisualStyleBackColor = true;
             this.view_visit.Click += new System.EventHandler(this.button1_Click);
             // 
-            // add_visit
-            // 
-            this.add_visit.Location = new System.Drawing.Point(35, 168);
-            this.add_visit.Name = "add_visit";
-            this.add_visit.Size = new System.Drawing.Size(157, 74);
-            this.add_visit.TabIndex = 1;
-            this.add_visit.Text = "Add a Visit";
-            this.add_visit.UseVisualStyleBackColor = true;
-            this.add_visit.Click += new System.EventHandler(this.button2_Click);
-            // 
             // books_button
             // 
-            this.books_button.Location = new System.Drawing.Point(399, 168);
+            this.books_button.Location = new System.Drawing.Point(292, 168);
             this.books_button.Name = "books_button";
             this.books_button.Size = new System.Drawing.Size(157, 74);
             this.books_button.TabIndex = 2;
@@ -69,7 +58,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(710, 436);
             this.Controls.Add(this.books_button);
-            this.Controls.Add(this.add_visit);
             this.Controls.Add(this.view_visit);
             this.Name = "admin_main_screen";
             this.Text = "Form2";
@@ -81,7 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Button view_visit;
-        private System.Windows.Forms.Button add_visit;
         private System.Windows.Forms.Button books_button;
     }
 }
